9ac4de75e6fd04c3a003772e7215b140d6f377b8
Milton Ouma <ouma.nyang1@gmail.com>
2022-08-03 18:04:05 +0300

    Completed

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,43 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 import { makeEmojiList } from "../utils";
 
 function ArticlePage() {
   // fetch data for a post
   const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
 
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
+  const url = `http://localhost:3000/posts/${id}`;
+
+  const {data: post, isLoaded} = useQuery(url)
+
 
   // set the document title
   const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
-
+  useDocumentTitle(pageTitle)
   if (!isLoaded) return <h3>Loading...</h3>;
 
   const { minutes, title, date, preview } = post;
   const emojis = makeEmojiList(minutes);
 
   return (
-    <article>
-      <h3>{title}</h3>
-      <small>
-        {date} • {emojis} {minutes} min read
-      </small>
-      <p>{preview}</p>
-    </article>
+      <article>
+        <h3>{title}</h3>
+        <small>
+          {date} • {emojis} {minutes} min read
+        </small>
+        <p>{preview}</p>
+      </article>
   );
 }
 
